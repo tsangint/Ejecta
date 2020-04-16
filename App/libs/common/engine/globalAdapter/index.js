a8ecb3f93e81836745053893676af4de71c110d9
luckyzeng <luckyzeng@tencent.com>
2020-04-16 14:22:22 +0800

    re init

--- App/libs/common/engine/globalAdapter/index.js
+++ App/libs/common/engine/globalAdapter/index.js
@@ -1,56 +1,7 @@
 'use strict';
 
 window.__globalAdapter = {
-    adaptView: function adaptView(viewProto) {
-        Object.assign(viewProto, {
-            _adjustViewportMeta: function _adjustViewportMeta() {
-                // minigame not support
-            },
-            setRealPixelResolution: function setRealPixelResolution(width, height, resolutionPolicy) {
-                // Reset the resolution size and policy
-                this.setDesignResolutionSize(width, height, resolutionPolicy);
-            },
-            enableAutoFullScreen: function enableAutoFullScreen(enabled) {
-                cc.warn('cc.view.enableAutoFullScreen() is not supported on minigame platform.');
-            },
-            isAutoFullScreenEnabled: function isAutoFullScreenEnabled() {
-                return false;
-            },
-            setCanvasSize: function setCanvasSize() {
-                cc.warn('cc.view.setCanvasSize() is not supported on minigame platform.');
-            },
-            setFrameSize: function setFrameSize() {
-                cc.warn('frame size is readonly on minigame platform.');
-            },
-            _initFrameSize: function _initFrameSize() {
-                var locFrameSize = this._frameSize;
-                if (__globalAdapter.isSubContext) {
-                    var sharedCanvas = window.sharedCanvas || __globalAdapter.getSharedCanvas();
-                    locFrameSize.width = sharedCanvas.width;
-                    locFrameSize.height = sharedCanvas.height;
-                } else {
-                    locFrameSize.width = window.innerWidth;
-                    locFrameSize.height = window.innerHeight;
-                }
-            }
-        });
-    },
+    adaptView: require('./View'),
 
-    adaptContainerStrategy: function adaptContainerStrategy(containerStrategyProto) {
-        containerStrategyProto._setupContainer = function (view, width, height) {
-            var locCanvas = cc.game.canvas;
-            // Setup pixel ratio for retina display
-            var devicePixelRatio = view._devicePixelRatio = 1;
-            if (view.isRetinaEnabled()) devicePixelRatio = view._devicePixelRatio = Math.min(view._maxPixelRatio, window.devicePixelRatio || 1);
-            // Setup canvas
-            width *= devicePixelRatio;
-            height *= devicePixelRatio;
-            // FIX: black screen on Baidu platform
-            // reset canvas size may call gl.clear(), especially when you call cc.director.loadScene()
-            if (locCanvas.width !== width || locCanvas.height !== height) {
-                locCanvas.width = width;
-                locCanvas.height = height;
-            }
-        };
-    }
+    adaptContainerStrategy: require('./ContainerStrategy')
 };
--- App/libs/wrapper/builtin/index.js
+++ App/libs/wrapper/builtin/index.js
@@ -16,7 +16,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
-var global = {};//GameGlobal;
+var global = GameGlobal;
 
 function inject() {
   _window.document = _document2.default;
@@ -71,7 +71,7 @@ function inject() {
   }
 }
 
-// if (!GameGlobal.__isAdapterInjected) {
-//   GameGlobal.__isAdapterInjected = true;
-//   inject();
-// }
+if (!GameGlobal.__isAdapterInjected) {
+  GameGlobal.__isAdapterInjected = true;
+  inject();
+}

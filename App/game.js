4b470294de6957e5f9e582d27b53762fcc95cbb2
luckyzeng <luckyzeng@tencent.com>
2020-04-17 10:40:04 +0800

    cocos2d-js no minify

--- App/game.js
+++ App/game.js
@@ -1,48 +1,4 @@
-if(typeof GameGlobal === "undefined") {
-    window.GameGlobal = window;
-    window.wx = {
-        getSystemInfoSync: function() {
-            return {"model":"iPhone 5","pixelRatio":2,"windowWidth":320,"windowHeight":568,"system":"iOS 10.0.1","language":"en","version":"7.0.4","screenWidth":320,"screenHeight":568,"SDKVersion":"2.11.0","brand":"devtools","fontSizeSetting":16,"benchmarkLevel":1,"batteryLevel":100,"statusBarHeight":20,"safeArea":{"right":320,"bottom":568,"left":0,"top":20,"width":320,"height":548},"platform":"devtools","devicePixelRatio":2}
-        },
-        onTouchStart: function() {},
-        onTouchMove: function() {},
-        onTouchEnd: function() {},
-        onTouchCancel: function() {},
-        createInnerAudioContext: function() {},
-        createVideo: function() {},
-        setPreferredFramesPerSecond: function() {},
-        showKeyboard: function() {},
-        hideKeyboard: function() {},
-        updateKeyboard: function() {},
-        onKeyboardInput: function() {},
-        onKeyboardConfirm: function() {},
-        onKeyboardComplete: function() {},
-        offKeyboardInput: function() {},
-        offKeyboardConfirm: function() {},
-        offKeyboardComplete: function() {},
-        getOpenDataContext: function() {},
-        onMessage: function() {},
-        loadSubpackage: function() {},
-        getSharedCanvas: function() {},
-        loadFont: function() {},
-        onShow: function() {},
-        onHide: function() {},
-        onError: function() {},
-        offError: function() {},
-
-        createImage: function() {
-            return function(){}
-        },
-        createCanvas: function() {
-            return new Ejecta.Canvas();
-        },
-
-        env: {"USER_DATA_PATH":"http://usr"},
-
-        getFileSystemManager: function(){}
-    }
-}
-
+require("./polyfill.js");
 require('./libs/wrapper/builtin/index');
 window.DOMParser = require('./libs/common/xmldom/dom-parser').DOMParser;
 require('./libs/common/engine/globalAdapter/index');
@@ -52,7 +8,7 @@ require('./libs/wrapper/systemInfo');
 window.__globalAdapter.init(function () {
     require('./src/settings');
     // Will be replaced with cocos2d-js path in editor
-    require('./cocos/cocos2d-js-min.js');
+    require('./cocos/cocos2d-js.js');
     require('./libs/common/engine/index');
     // Introduce Cocos Service here
     require('./main');
@@ -71,7 +27,6 @@ window.__globalAdapter.init(function () {
     cc.loader.insertPipeAfter(pipeBeforeDownloader, remoteDownloader);
 
     if (cc.sys.platform === cc.sys.WECHAT_GAME_SUB) {
-        // TODO(lucky)
         // var SUBDOMAIN_DATA = require('src/subdomain.json.js');
         // cc.game.once(cc.game.EVENT_ENGINE_INITED, function () {
         //     cc.Pipeline.Downloader.PackDownloader._doPreload("SUBDOMAIN_DATA", SUBDOMAIN_DATA);
